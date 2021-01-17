06febd324c39f652cfe8649bbe7121a91b0b9753
Ashish <ar.sawan16@gmail.com>
2021-01-18 00:57:54 +0530

    added region variable

--- terraform-modules/vpc/variables.tf
+++ terraform-modules/vpc/variables.tf
@@ -2,6 +2,9 @@ variable "name" {
 
 }
 
+variable "aws_region" {
+  
+}
 variable "enable_dns_support" {
   description = "should be true if you want to use private DNS within the VPC"
   default = false
